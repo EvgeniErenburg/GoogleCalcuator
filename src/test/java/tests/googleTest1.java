964c42ed161d0687b9adae4e76e601f3493d6cc5
Evgeni_Erenburg <erenburg_evgeni@mail.ru>
2022-02-13 18:42:55 +0400

    Калькулятор

--- src/test/java/tests/googleTest1.java
+++ src/test/java/tests/googleTest1.java
@@ -1,18 +1,38 @@
 package tests;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class googleTest1 {
 
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void init (){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
     @Test
     public void test1(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://google.com");
-        driver.quit();
 
+        driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("калькулятор", Keys.ENTER);
+
+
+    }
+
+    @AfterAll
+    public static void teardown(){
+        driver.quit();
     }
 
 }
